4f9cbe103fa93d6859aed56b2360b8323d17dbd1
mr.myatthu <myatthu@comquas.com>
2020-07-09 15:46:15 +0630

    Chat init

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Chat from "../views/Chat.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +10,12 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
     path: "/about",
@@ -17,14 +24,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: Chat,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>Please Login with your google account to continue.</h3>
+
+    <button @click="login">Login with Google</button>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  methods: {
+    login() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          console.log(result);
+          //   var token = result.credential.accessToken;
+          //   var user = result.user;
+
+          this.$router.push("/chat");
+        })
+        .catch(function(error) {
+          console.log(error);
+
+          //   var errorCode = error.code;
+          //   var errorMessage = error.message;
+          //   var email = error.email;
+          //   var credential = error.credential;
+        });
+    },
+  },
+};
+</script>
